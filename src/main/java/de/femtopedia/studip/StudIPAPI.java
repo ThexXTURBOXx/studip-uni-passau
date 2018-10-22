71bb0bfda5b8583bc503bc82dd6c8f3d507b2d59
Nico Mexis <nico@bellaza.net>
2018-10-23 01:02:25 +0200

    Changed to Google's HTTPClient

--- src/main/java/de/femtopedia/studip/StudIPAPI.java
+++ src/main/java/de/femtopedia/studip/StudIPAPI.java
@@ -1,5 +1,6 @@
 package de.femtopedia.studip;
 
+import com.google.api.client.http.apache.ApacheHttpTransport;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import de.femtopedia.studip.json.Contacts;
@@ -14,15 +15,13 @@ import de.femtopedia.studip.shib.ShibbolethClient;
 import java.io.IOException;
 import java.io.InputStream;
 import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.util.EntityUtils;
+import org.apache.http.HttpResponse;
 
 public class StudIPAPI {
 
 	private static final String BASE_URL = "https://studip.uni-passau.de/studip/api.php/";
 	private ShibbolethClient sc;
-	private CloseableHttpClient client;
+	private ApacheHttpTransport client;
 	private Gson gson;
 
 	public StudIPAPI(String username, String password) throws IOException, IllegalArgumentException, IllegalAccessException, IllegalStateException {
@@ -36,7 +35,7 @@ public class StudIPAPI {
 		return this.sc;
 	}
 
-	public CloseableHttpClient getClient() {
+	public ApacheHttpTransport getClient() {
 		return this.client;
 	}
 
@@ -47,98 +46,68 @@ public class StudIPAPI {
 		return result.toString();
 	}
 
-	public CloseableHttpResponse get(String url) throws IOException {
+	public HttpResponse get(String url) throws IOException {
 		return this.sc.get(BASE_URL + url);
 	}
 
 	public User getCurrentUserData() throws IOException {
-		CloseableHttpResponse r = this.get("user");
+		HttpResponse r = this.get("user");
 		HttpEntity e = r.getEntity();
-		User user = gson.fromJson(getString(e.getContent()), User.class);
-		EntityUtils.consume(e);
-		r.close();
-		return user;
+		return gson.fromJson(getString(e.getContent()), User.class);
 	}
 
 	public User getUserData(String userID) throws IOException {
-		CloseableHttpResponse r = this.get("user/" + userID);
+		HttpResponse r = this.get("user/" + userID);
 		HttpEntity e = r.getEntity();
-		User user = gson.fromJson(getString(e.getContent()), User.class);
-		EntityUtils.consume(e);
-		r.close();
-		return user;
+		return gson.fromJson(getString(e.getContent()), User.class);
 	}
 
 	public Contacts getContacts(String userID) throws IOException {
-		CloseableHttpResponse r = this.get("user/" + userID + "/contacts");
+		HttpResponse r = this.get("user/" + userID + "/contacts");
 		HttpEntity e = r.getEntity();
-		Contacts contacts = gson.fromJson(getString(e.getContent()), Contacts.class);
-		EntityUtils.consume(e);
-		r.close();
-		return contacts;
+		return gson.fromJson(getString(e.getContent()), Contacts.class);
 	}
 
 	public Events getEvents(String userID) throws IOException {
-		CloseableHttpResponse r = this.get("user/" + userID + "/events");
+		HttpResponse r = this.get("user/" + userID + "/events");
 		HttpEntity e = r.getEntity();
-		Events events = gson.fromJson(getString(e.getContent()), Events.class);
-		EntityUtils.consume(e);
-		r.close();
-		return events;
+		return gson.fromJson(getString(e.getContent()), Events.class);
 	}
 
 	public Course getCourse(String courseID) throws IOException {
-		CloseableHttpResponse r = this.get("course/" + courseID);
+		HttpResponse r = this.get("course/" + courseID);
 		HttpEntity e = r.getEntity();
-		Course course = gson.fromJson(getString(e.getContent()), Course.class);
-		EntityUtils.consume(e);
-		r.close();
-		return course;
+		return gson.fromJson(getString(e.getContent()), Course.class);
 	}
 
 	public Courses getCourses(String userID) throws IOException {
-		CloseableHttpResponse r = this.get("user/" + userID + "/courses");
+		HttpResponse r = this.get("user/" + userID + "/courses");
 		HttpEntity e = r.getEntity();
-		Courses courses = gson.fromJson(getString(e.getContent()), Courses.class);
-		EntityUtils.consume(e);
-		r.close();
-		return courses;
+		return gson.fromJson(getString(e.getContent()), Courses.class);
 	}
 
 	public Semesters getSemesters() throws IOException {
-		CloseableHttpResponse r = this.get("semesters");
+		HttpResponse r = this.get("semesters");
 		HttpEntity e = r.getEntity();
-		Semesters semesters = gson.fromJson(getString(e.getContent()), Semesters.class);
-		EntityUtils.consume(e);
-		r.close();
-		return semesters;
+		return gson.fromJson(getString(e.getContent()), Semesters.class);
 	}
 
 	public Semester getSemester(String semesterID) throws IOException {
-		CloseableHttpResponse r = this.get("semester/" + semesterID);
+		HttpResponse r = this.get("semester/" + semesterID);
 		HttpEntity e = r.getEntity();
-		Semester semester = gson.fromJson(getString(e.getContent()), Semester.class);
-		EntityUtils.consume(e);
-		r.close();
-		return semester;
+		return gson.fromJson(getString(e.getContent()), Semester.class);
 	}
 
 	public Schedule getSchedule(String userID) throws IOException {
-		CloseableHttpResponse r = this.get("user/" + userID + "/schedule");
+		HttpResponse r = this.get("user/" + userID + "/schedule");
 		HttpEntity e = r.getEntity();
-		Schedule schedule = gson.fromJson(getString(e.getContent()), Schedule.class);
-		EntityUtils.consume(e);
-		r.close();
-		return schedule;
+		return gson.fromJson(getString(e.getContent()), Schedule.class);
 	}
 
 	public Schedule getSchedule(String userID, String semesterID) throws IOException {
-		CloseableHttpResponse r = this.get("user/" + userID + "/schedule/" + semesterID);
+		HttpResponse r = this.get("user/" + userID + "/schedule/" + semesterID);
 		HttpEntity e = r.getEntity();
-		Schedule schedule = gson.fromJson(getString(e.getContent()), Schedule.class);
-		EntityUtils.consume(e);
-		r.close();
-		return schedule;
+		return gson.fromJson(getString(e.getContent()), Schedule.class);
 	}
 
 }
